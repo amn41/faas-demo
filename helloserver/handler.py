0bf35349497a514d723f582ff1697af1b4f2a51e
Alan Nichol <alan@rasa.com>
2019-03-26 21:46:03 +0100

    serve some dummy html

--- helloserver/handler.py
+++ helloserver/handler.py
@@ -4,4 +4,16 @@ def handle(req):
         req (str): request body
     """
 
-    return req
+    html = """
+<!DOCTYPE html>
+<html>
+<body>
+
+<!-- This is a comment -->
+<p>This is a great webpage.</p>
+<!-- Comments are not displayed in the browser -->
+
+</body>
+</html>
+    """
+    return html
